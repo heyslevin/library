e7a9745378483696cc4e29cf9cf2d3b992a761ff
memellaguno <memellaguno@gmail.com>
2020-04-12 16:36:12 -0500

    Delete works correctly

--- script.js
+++ script.js
@@ -5,8 +5,6 @@ let add = document.querySelector("#add")
 let submit = document.querySelector("#submit")
 let popupWindow = document.querySelector(".pop-container")
 let table = document.querySelector(".table");
-let delbutton = []
-
 
 // Book creator
 function Book(title,author,pages,read) {
@@ -27,16 +25,29 @@ function addIndex() {
 
 // Button Generator 
 
-let newButton = document.createElement("button")
+function buttonGenerator(row,id) {
+
+	let rowId = id
+	let buttonCell = document.createElement("td")
+	let newButton = document.createElement("button")
+	newButton.setAttribute("id","delete")
+	newButton.innerHTML = "Delete"
+	newButton.addEventListener("click",deleteBook)
+	newButton.setAttribute("data-rowId",rowId)
+
+	buttonCell.appendChild(newButton)
+	row.appendChild(buttonCell)
+}
+
 
 // Add to library array + table
 function addtoLibrary(title,author,pages,read) {
 	let book = new Book(title,author,pages,read)
 	myLibrary.push(book)
-	addIndex()
-	generateTable(book)
-	delbutton = document.querySelectorAll('button[id="delete"]')
-	delbutton.forEach(e => e.addEventListener("click",deleteBook));
+	cleartable()
+	generateTable(myLibrary)
+	// delButton = document.querySelectorAll('button[id="delete"]')
+	// delButton.forEach(e => e.addEventListener("click",deleteBook));
 }
 
 
@@ -46,46 +57,51 @@ let book1 = addtoLibrary("Dick tales 2, bigger","Richard Hawkins",2500,"No")
 let book2 = addtoLibrary("Dick tales 3, the revenge","Richard Hawkins",300,"No")
 let book3 = addtoLibrary("Dick tales 4, coming up","Richard Hawkins",210,"No")
 
-// Table generator
 
 
+// Table generator ****************
 
 function generateTable(arr) {
 
+
+	arr.forEach(function(e) { 
+
 	let row = document.createElement("tr")
 
+		
+		for (var key in e) {
+			let rowData = document.createElement("td")
 
-	for (var key in arr) {
-		let rowData = document.createElement("td")
+			obj = e[key]
+			rowData.innerHTML = obj
+			row.appendChild(rowData)
+			table.appendChild(row)
+		}
+	
 
-		obj = arr[key]
-		rowData.innerHTML = obj
-		row.appendChild(rowData)
+	addIndex()
+	let rowId = e['id']
+	row.setAttribute("id",arr['id'])
+	buttonGenerator(row,rowId)
 
-		table.appendChild(row)
-	}
+	});
+
 
 }
 
+//Clear table
 
-
-// function generatetable(e) {
-// 	table.innerHTML += `<tr data-attribute="${e.id}"><td>${e.title}</td>
-// 							<td>${e.author}</td>
-// 							<td>${e.pages}</td>
-// 							<td>${e.read}</td>
-// 							<td></td>
-// 						</tr>
-// 						`
-// }
-
-
+function cleartable() {
+	table.innerHTML = ""
+}
 
 //Button actions
 
-function deleteBook(btn) {
-	let row = btn.parentNode;
-	alert(btn)
+function deleteBook(arr) {
+	index = this.dataset.rowid
+	myLibrary.splice(index,1)
+	cleartable()
+	generateTable(myLibrary)
 }
 
 // Add new book actions
